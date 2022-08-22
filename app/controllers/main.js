eb4189105e2d35f58f3f9781e84ce4884ab2b05a
redtnx <red.tnx@gmail.com>
2022-08-22 20:48:10 +0700

    complete

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -30,9 +30,100 @@ function renderHTML(data) {
             <img width="50px" src="./../../assets/img/${product.hinhAnh}">
         </td>
         <td>${product.moTa}</td>
+        <td>
+        <button class="btn btn-info" onclick="editProduct(${
+          product.id
+        })" data-toggle="modal" data-target="#myModal">Edit</button>
+
+        <button class="btn btn-danger" onclick="deleteProduct(${
+          product.id
+        })">Xóa</button>
     </tr>
     `;
   });
 
   getEle("tblDanhSachSP").innerHTML = content;
+}
+
+// Xóa
+function deleteProduct(id) {
+  service
+    .deleteProductApi(id)
+    .then(function () {
+      fetchData();
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+// Thêm
+getEle("btnThemSP").addEventListener("click", function () {
+  // Sửa title
+  document.getElementsByClassName("modal-title")[0].innerHTML = "Thêm sản phẩm";
+  // Tạo nút Add
+  var btnAdd = `<button class = "btn btn-success" onclick="addProduct()">Add</button>`;
+  document.getElementsByClassName("modal-footer")[0].innerHTML = btnAdd;
+});
+
+function addProduct() {
+  var tenSP = getEle("TenSP").value;
+  var giaSP = getEle("GiaSP").value;
+  var hinhSP = getEle("HinhSP").value;
+  var moTa = getEle("MoTa").value;
+
+  var product = new Product("", tenSP, giaSP, hinhSP, moTa);
+
+  service
+    .addProductApi(product)
+    .then(function () {
+      fetchData();
+      // Tắt modal
+      document.getElementsByClassName("close")[0].click();
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+// Sửa
+function editProduct(id) {
+  // Sửa title
+  document.getElementsByClassName("modal-title")[0].innerHTML =
+    "Cập nhật sản phẩm";
+  // Tạo nút Update
+  var btnUpdate = `<button class="btn btn-info" onclick="updateProduct(${id})")>Update</button>`;
+  document.getElementsByClassName("modal-footer")[0].innerHTML = btnUpdate;
+
+  service
+    .getProduct(id)
+    .then(function (result) {
+      getEle("TenSP").value = result.data.tenSP;
+      getEle("GiaSP").value = result.data.gia;
+      getEle("HinhSP").value = result.data.hinhAnh;
+      getEle("MoTa").value = result.data.moTa;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+// Update
+function updateProduct(id) {
+  var tenSP = getEle("TenSP").value;
+  var giaSP = getEle("GiaSP").value;
+  var hinhSP = getEle("HinhSP").value;
+  var moTa = getEle("MoTa").value;
+
+  var product = new Product(id, tenSP, giaSP, hinhSP, moTa);
+
+  service
+    .updateProductApi(product)
+    .then(function () {
+      fetchData();
+      document.getElementsByClassName("close")[0].click();
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
--- app/services/services.js
+++ app/services/services.js
@@ -5,4 +5,34 @@ function Services() {
       method: "GET",
     });
   };
+
+  this.deleteProductApi = function (id) {
+    return axios({
+      url: `https://62ff793c9350a1e548df750f.mockapi.io/api/Products/${id}`,
+      method: "DELETE",
+    });
+  };
+
+  this.addProductApi = function (product) {
+    return axios({
+      url: "https://62ff793c9350a1e548df750f.mockapi.io/api/Products",
+      method: "POST",
+      data: product,
+    });
+  };
+
+  this.getProduct = function (id) {
+    return axios({
+      url: `https://62ff793c9350a1e548df750f.mockapi.io/api/Products/${id}`,
+      method: "GET",
+    });
+  };
+
+  this.updateProductApi = function (product) {
+    return axios({
+      url: `https://62ff793c9350a1e548df750f.mockapi.io/api/Products/${product.id}`,
+      method: "PUT",
+      data: product,
+    });
+  };
 }
